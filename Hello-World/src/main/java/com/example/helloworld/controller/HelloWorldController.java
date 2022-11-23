8ea814b368f2e536101027bee07f420ad5ac2c44
Fco Gaz <fcogaz@icloud.com>
2022-11-22 21:58:02 -0600

    1 commit

---
+++ Hello-World/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.helloworld.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+	
+	@GetMapping
+	public ResponseEntity<String> helloWorld(){
+		
+		return new ResponseEntity <>("Welcome to Spring again !!!", HttpStatus.OK);
+	}
+	
+
+}
